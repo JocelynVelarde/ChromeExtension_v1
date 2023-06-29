704f4d6bfdd11962186feac000896fa6849d1a05
Jocelyn Velarde <a01285780@itesm.mx>
2023-06-29 16:53:13 -0600

    simple button that detects linkedin page v1

--- popup.js
+++ popup.js
@@ -1,17 +1,20 @@
-// Send a message to the content script to retrieve the profile name
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    const activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { action: 'getProfileName' }, function(response) {
-      const profileName = response.profileName;
-      console.log('Profile Name:', profileName);
-      sendProfileNameToBackgroundScript(profileName);
+document.addEventListener("DOMContentLoaded", function () {
+    var linkedinButton = document.getElementById("linkedinButton");
+    var messageText = document.getElementById("messageText");
+  
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var currentTab = tabs[0];
+      var currentUrl = currentTab.url;
+  
+      // Verificar si la URL es de LinkedIn
+      if (currentUrl.includes("linkedin.com/in/")) {
+        linkedinButton.addEventListener("click", function () {
+          messageText.textContent = "You have clicked the button";
+        });
+      } else {
+        linkedinButton.disabled = true;
+        messageText.textContent = "This is not a LinkedIn profile";
+      }
     });
   });
-  
-  // Send the profile name to the background script
-  function sendProfileNameToBackgroundScript(profileName) {
-    chrome.runtime.sendMessage({ profileName }, function(response) {
-      console.log('Message sent to background script:', response);
-    });
-  }
   
