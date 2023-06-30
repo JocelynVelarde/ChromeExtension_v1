1aec29cf18c3cf691b326b27c62c3517a93e50af
Jocelyn Velarde <a01285780@itesm.mx>
2023-06-30 09:25:44 -0600

    crm button displays name, experience in alert v3

--- contentScript.js
+++ contentScript.js
@@ -1,40 +1,37 @@
 const init = function() {
-    // Find the profile name element on LinkedIn
-    const profileNameElement = document.querySelector('.pv-top-card-v2-ctas');
-  
-    if (profileNameElement) {
-      // Create the injected button element
-      const injectElement = document.createElement('button');
-      injectElement.className = 'button-crm';
-  
-      // Apply additional CSS styles to the button
-      injectElement.style.border = 'none';
-      injectElement.style.borderRadius = '16px';
-      injectElement.style.padding = '8px 16px';
-      injectElement.style.background = '#0a66c2';
-      injectElement.style.color = '#fff';
-      injectElement.style.fontWeight = 'bold';
-      injectElement.style.marginTop = '16px';
-  
-      // Create the span element for the icon
-      const iconSpan = document.createElement('span');
-      iconSpan.className = 'plus-icon';
-      iconSpan.innerHTML = '&#43;'; // Plus sign icon
-  
-      // Apply styles to the icon span
-      iconSpan.style.fontSize = '17px'; // Adjust the font size as needed
-  
-      // Append the icon span to the button element
-      injectElement.appendChild(iconSpan);
-  
-      // Add the text to the button
-      injectElement.innerHTML += ' Add to CRM';
-  
-      // Append the injected button element as a sibling to the profile name element
-      profileNameElement.parentNode.insertBefore(injectElement, profileNameElement.nextSibling);
-    }
-  };
-  
-  // Call the init function
-  init();
-  
+  const profileNameElement = document.querySelector('.pv-top-card-v2-ctas');
+  const nombrePersonaElement = document.querySelector('.pv-text-details__left-panel');
+  const nombrePersona = nombrePersonaElement ? nombrePersonaElement.textContent.trim() : null;
+
+  if (profileNameElement) {
+    const injectElement = document.createElement('button');
+    injectElement.className = 'button-crm';
+
+    injectElement.style.border = 'none';
+    injectElement.style.borderRadius = '16px';
+    injectElement.style.padding = '8px 16px';
+    injectElement.style.background = '#0a66c2';
+    injectElement.style.color = '#fff';
+    injectElement.style.fontWeight = 'bold';
+    injectElement.style.marginTop = '16px';
+
+    const iconSpan = document.createElement('span');
+    iconSpan.className = 'plus-icon';
+    iconSpan.innerHTML = '&#43;';
+
+    iconSpan.style.fontSize = '17px';
+
+    injectElement.appendChild(iconSpan);
+
+    injectElement.innerHTML += ' Add to CRM';
+
+    injectElement.addEventListener('click', function() {
+      alert('Profile Name: ' + nombrePersona);
+    });
+
+    profileNameElement.parentNode.insertBefore(injectElement, profileNameElement.nextSibling);
+  }
+};
+
+// Call the init function
+init();
