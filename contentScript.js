bd123c0f9759144c506c0ee808c89f0657a1b666
Jocelyn Velarde <a01285780@itesm.mx>
2023-06-30 10:12:09 -0600

    added pronoun field to profiles that have them v5

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,5 @@
 const init = function() {
   const profileNameElement = document.querySelector('.pv-top-card-v2-ctas');
-  const nombrePersonaElement = document.querySelector('.pv-text-details__left-panel');
-  const nombrePersona = nombrePersonaElement ? nombrePersonaElement.textContent.trim() : null;
 
   if (profileNameElement) {
     const injectElement = document.createElement('button');
@@ -26,8 +24,12 @@ const init = function() {
     injectElement.innerHTML += ' Add to CRM';
 
     injectElement.addEventListener('click', function() {
+      const nombrePersonaElement = document.querySelector('.pv-text-details__left-panel');
+      const nombrePersona = nombrePersonaElement ? nombrePersonaElement.textContent.trim() : null;
+
       const profileInfo = separateProfileInfo(nombrePersona);
       alert('Profile Name: ' + profileInfo.nombrePersona);
+      alert('Pronouns: ' + profileInfo.pronouns);
       alert('Connection: ' + profileInfo.connection);
       alert('Headline: ' + profileInfo.headline);
     });
@@ -37,13 +39,27 @@ const init = function() {
 };
 
 function separateProfileInfo(nombrePersona) {
-  const parts = nombrePersona.split(/\s{2,}/);
-  const name = parts[0].trim();
-  const connection = parts[1].trim();
-  const headline = parts[3].trim();
+  const parts = nombrePersona.split('\n').map(line => line.trim());
+  const name = parts[0];
+
+  let pronouns = '';
+  let connection = '';
+  let headline = '';
+
+  for (let i = 1; i < parts.length; i++) {
+    const line = parts[i];
+    if (line.includes('he/him') || line.includes('she/her') || line.includes('they/them') || line.includes('ze/zir') || line.includes('xe/xem') || line.includes('other')) {
+      pronouns = line;
+    } else if (line.includes('1st degree connection') || line.includes('2nd degree connection') || line.includes('3rd')) {
+      connection = line;
+    } else {
+      headline = line;
+    }
+  }
 
   return {
     nombrePersona: name,
+    pronouns: pronouns,
     connection: connection,
     headline: headline
   };
